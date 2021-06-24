2a2bb2cac01ac3ca81cb132ba411d8e9972f165d
stephane betton <stphane.betton@ag2rlamondiale.fr>
2021-06-24 13:41:04 +0200

    init

---
+++ src/test/java/com/stephane/strategy/pattern/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephane.strategy.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
